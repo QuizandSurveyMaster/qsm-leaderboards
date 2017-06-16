f625817ad0ea2c53c6aff03f3ccb7554dfc9401e
Frank Corso <frank@mylocalwebstop.com>
2017-06-16 18:48:23 -0400

    Creates addon settings tab

--- php/addon-settings-tab-content.php
+++ php/addon-settings-tab-content.php
@@ -7,11 +7,11 @@ if ( ! defined( 'ABSPATH' ) ) exit;
  * @since 0.1.0
  * @return void
  */
-function qsm_addon_xxxxx_register_addon_settings_tabs() {
-  global $mlwQuizMasterNext;
-  if ( ! is_null( $mlwQuizMasterNext ) && ! is_null( $mlwQuizMasterNext->pluginHelper ) && method_exists( $mlwQuizMasterNext->pluginHelper, 'register_quiz_settings_tabs' ) ) {
-    $mlwQuizMasterNext->pluginHelper->register_addon_settings_tab( "Plugin Name", 'qsm_addon_xxxxx_addon_settings_tabs_content' );
-  }
+function qsm_addon_leaderboards_register_addon_settings_tabs() {
+	global $mlwQuizMasterNext;
+	if ( ! is_null( $mlwQuizMasterNext ) && ! is_null( $mlwQuizMasterNext->pluginHelper ) && method_exists( $mlwQuizMasterNext->pluginHelper, 'register_quiz_settings_tabs' ) ) {
+		$mlwQuizMasterNext->pluginHelper->register_addon_settings_tab( "Leaderboards", 'qsm_addon_leaderboard_addon_settings_tabs_content' );
+	}
 }
 
 /**
@@ -21,31 +21,27 @@ function qsm_addon_xxxxx_register_addon_settings_tabs() {
  * @return void
  * @todo Replace the xxxxx with your addon's name
  */
-function qsm_addon_xxxxx_addon_settings_tabs_content() {
-
-  //Enqueue your scripts and styles
-  wp_enqueue_script( 'plugin_name_admin_script', plugins_url( '../js/plugin-name-admin.js' , __FILE__ ), array( 'jquery' ) );
-  wp_enqueue_style( 'plugin_name_admin_style', plugins_url( '../css/plugin-name-admin.css' , __FILE__ ) );
+function qsm_addon_leaderboard_addon_settings_tabs_content() {
 
 	global $mlwQuizMasterNext;
 
-  //If nonce is correct, update settings from passed input
-  if ( isset( $_POST["analysis_nonce"] ) && wp_verify_nonce( $_POST['analysis_nonce'], 'analysis') ) {
+	//If nonce is correct, update settings from passed input
+	if ( isset( $_POST["leaderboards_nonce"] ) && wp_verify_nonce( $_POST['leaderboards_nonce'], 'leaderboards') ) {
 
-    // Load previous license key
-    $analysis_data = get_option( 'qsm_addon_analysis_settings', '' );
-    if ( isset( $logic_data["license_key"] ) ) {
-      $license = trim( $analysis_data["license_key"] );
+		// Load previous license key
+    $leaderboard_data = get_option( 'qsm_addon_leaderboard_settings', '' );
+    if ( isset( $leaderboard_data["license_key"] ) ) {
+      $license = trim( $leaderboard_data["license_key"] );
     } else {
       $license = '';
     }
 
     // Save settings
     $saved_license = sanitize_text_field( $_POST["license_key"] );
-    $analysis_data = array(
+    $leaderboard_data = array(
       'license_key' => $saved_license
     );
-    update_option( 'qsm_addon_analysis_settings', $analysis_data );
+    update_option( 'qsm_addon_leaderboard_settings', $leaderboard_data );
 
     // Checks to see if the license key has changed
     if ( $license != $saved_license ) {
@@ -54,12 +50,12 @@ function qsm_addon_xxxxx_addon_settings_tabs_content() {
       $api_params = array(
         'edd_action'=> 'activate_license',
         'license' 	=> $saved_license,
-        'item_name' => urlencode( 'Reporting & Analysis' ), // the name of our product in EDD
+        'item_name' => urlencode( 'Leaderboards' ), // the name of our product in EDD
         'url'       => home_url()
       );
 
       // Call the custom API.
-      $response = wp_remote_post( 'http://quizandsurveymaster.com', array( 'timeout' => 15, 'sslverify' => false, 'body' => $api_params ) );
+      $response = wp_remote_post( 'https://quizandsurveymaster.com', array( 'timeout' => 15, 'sslverify' => false, 'body' => $api_params ) );
 
       // If previous license key was entered
       if ( ! empty( $license ) ) {
@@ -68,7 +64,7 @@ function qsm_addon_xxxxx_addon_settings_tabs_content() {
         $api_params = array(
           'edd_action'=> 'deactivate_license',
           'license' 	=> $license,
-          'item_name' => urlencode( 'Reporting & Analysis' ), // the name of our product in EDD
+          'item_name' => urlencode( 'Leaderboards' ), // the name of our product in EDD
           'url'       => home_url()
         );
 
@@ -80,11 +76,11 @@ function qsm_addon_xxxxx_addon_settings_tabs_content() {
   }
 
   // Load settings
-  $analysis_data = get_option( 'qsm_addon_analysis_settings', '' );
-  $analysis_defaults = array(
+  $leaderboard_data = get_option( 'qsm_addon_leaderboard_settings', '' );
+  $leaderboard_defaults = array(
     'license_key' => ''
   );
-  $analysis_data = wp_parse_args( $analysis_data, $analysis_defaults );
+  $leaderboard_data = wp_parse_args( $leaderboard_data, $leaderboard_defaults );
 
   // Show any alerts from saving
   $mlwQuizMasterNext->alertManager->showAlerts();
@@ -94,10 +90,10 @@ function qsm_addon_xxxxx_addon_settings_tabs_content() {
     <table class="form-table" style="width: 100%;">
       <tr valign="top">
         <th scope="row"><label for="license_key">Addon License Key</label></th>
-        <td><input type="text" name="license_key" id="license_key" value="<?php echo $analysis_data["license_key"]; ?>"></td>
+        <td><input type="text" name="license_key" id="license_key" value="<?php echo $leaderboard_data["license_key"]; ?>"></td>
       </tr>
     </table>
-    <?php wp_nonce_field('analysis','analysis_nonce'); ?>
+    <?php wp_nonce_field('leaderboards','leaderboards_nonce'); ?>
     <button class="button-primary">Save Changes</button>
   </form>
   <?php
