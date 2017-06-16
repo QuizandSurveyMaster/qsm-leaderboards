c08a14f0cd7e99b74962be678e74eee23a5ccc8d
Frank Corso <frank@mylocalwebstop.com>
2017-06-16 19:12:37 -0400

    Creates shortcode

--- php/shortcodes.php
+++ php/shortcodes.php
@@ -1,12 +1,20 @@
 <?php
 
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+/**
+ * Converts shortcode to HTML
+ *
+ * @since 1.0.0
+ * @param $atts Array of shortcode parameters
+ * @return string The HTML for the shortcode
+ */
 function qsm_addon_leaderboards_shortcode( $atts ) {
 	extract(shortcode_atts(array(
-		'mlw_quiz' => 0
+		'quiz' => 0
 	), $atts));
-	$mlw_quiz_id = intval( $mlw_quiz );
-	$mlw_quiz_leaderboard_display = "";
+	$quiz_id = intval( $quiz );
 
-	// Do leaderboard function
+	return qsm_addon_leaderboards_generate( $quiz_id );
 }
 ?>
